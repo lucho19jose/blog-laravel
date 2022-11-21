c7b844b6016573e2bd49f1c8fff2e5b334506dee
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-21 05:52:34 -0500

    destroy post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,7 +18,15 @@
                         <a href="" class="text-indigo-600">Editar</a>
                       </td>
                       <td class="px-6 py-4">
-                        <a href="" class="text-indigo-600">Eliminar</a>
+                        <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                          @csrf
+                          @method('DELETE')
+                          <input 
+                            type="submit" 
+                            value="Eliminar" 
+                            class="bg-gray-800 text-white rounded px-4 py-2 cursor-pointer"
+                            onclick="return confirm('Desea eliminar?')">
+                        </form>
                       </td>
                     </tr>
                   @empty
