2acf656fc2be562ba425f97149fd95fba57825aa
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-21 15:49:26 -0500

    redirect dasboard to posts

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,10 @@ use App\Http\Controllers\PostController;
 
 Route::controller(PageController::class)->group(function () {
     Route::get('/',                 'home')->name('home');
-    Route::get('/blog',             'blog')->name('blog');
     Route::get('/blog/{post:slug}', 'post')->name('post');
  });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('dashboard', 'posts')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -33,7 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('posts', PostController::class)->except(['show']);
+Route::resource('posts', PostController::class)->middleware(['auth'])->except(['show']);
 
 require __DIR__.'/auth.php';
 
