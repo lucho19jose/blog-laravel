9234675bf546f95080808dec4862914054cb90f2
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-21 03:54:49 -0500

    init with blog

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::get('/', function () {
+    return view('welcome');
+});
+ */
+
+ Route::controller(PageController::class)->group(function () {
+    Route::get('/',                 'home')->name('home');
+    Route::get('/blog',             'blog')->name('blog');
+    Route::get('/blog/{post:slug}', 'post')->name('post');
+ });
