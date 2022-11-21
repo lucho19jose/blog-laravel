84d4f817f4b7a497de00aa25fb0609e14e28d528
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-21 05:05:15 -0500

    login with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
-    return view('welcome');
-});
- */
-
- Route::controller(PageController::class)->group(function () {
+Route::controller(PageController::class)->group(function () {
     Route::get('/',                 'home')->name('home');
     Route::get('/blog',             'blog')->name('blog');
     Route::get('/blog/{post:slug}', 'post')->name('post');
  });
+ 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
