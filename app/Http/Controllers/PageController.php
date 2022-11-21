2ac48d65c5a27049a90e36256bfd0bbcedc0f6e5
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-21 13:49:25 -0500

    add author

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -17,13 +17,17 @@ class PageController extends Controller
         //
     }
     
-    public function home()
+    public function home(Request $request)
     {
-        $posts = Post::latest()->paginate();
+        $search = $request->search;
+
+        $posts = Post::where('title', 'LIKE', "%{$search}%")
+            ->with('user')
+            ->latest()->paginate();
 
         return view('home', ['posts' => $posts]);
     }
-    
+
     public function post(Post $post)
     {
         return view('post', ['post' => $post]);
